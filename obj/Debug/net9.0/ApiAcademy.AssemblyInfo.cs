e4abebceb85bf898c8e48b379643b42c5374d5aa
EmersonJun <EmersonnJunior2006@gmail.com>
2025-10-02 20:11:48 -0300

    add

--- obj/Debug/net9.0/ApiAcademy.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiAcademy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiAcademy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0f973933225e0ad52a9684fa7914110e7c46f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bc23cd128729af973bf73a5dffd1203b4cbf7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiAcademy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiAcademy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
